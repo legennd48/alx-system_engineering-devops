c7234e0e62b98a7e8c23d782d2756564758b01c6
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-31 20:51:10 +0000

    initial commit

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Module: Repetition Token #3
+
+puts ARGV[0].scan(/hb(t?)+n/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Module: Repetition Token #3
+
+puts ARGV[0].scan(/^h[a-zA-Z0-9]n/).join

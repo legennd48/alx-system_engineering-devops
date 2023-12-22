90ae485697b8a9f0cd19d4cc98c6d0cf261f411d
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-22 16:27:48 +0000

    added installation of dependencies for flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,23 @@
-# Using Puppet to install Flask from pip3
+# Using Puppet to install Flask and its dependencies from pip3
 
-package {'flask':
+# Install Python 3
+package { 'python3':
+  ensure => present,
+}
+
+# Install Python 3 pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }

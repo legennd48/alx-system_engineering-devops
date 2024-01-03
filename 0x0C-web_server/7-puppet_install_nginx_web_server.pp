29246d6efb21ef1f9d733cdbdac14a7853e2571a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-03 12:23:12 +0000

    changes to puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,25 @@
 # Setup New Ubuntu server with nginx
 
-exec { 'install_nginx':
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
-  command  => 'sudo apt-get -y update' +
-              ' && sudo apt-get -y install nginx' +
-              ' && echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html' +
-              ' && sudo sed -i "/server_name _;/a\ \n\tlocation /redirect_me {\n\t\treturn 301 https://github.com;\n\t}" /etc/nginx/sites-available/default' +
-              ' && sudo service nginx start',
+}
+package {'nginx':
+  ensure => 'installed',
+}
+
+exec {'Hello_world':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+  require  => Package['nginx'],
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.github.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }

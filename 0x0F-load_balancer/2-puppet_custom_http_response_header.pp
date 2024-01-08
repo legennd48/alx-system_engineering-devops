72bfa0339b65cecf929e42648d1ffc240ed4a57e
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-08 19:21:22 +0000

    initial commit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,15 @@
 # Update the package list
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+  command => '/usr/bin/apt-get update',
 }
-
-# Install Nginx
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Add custom HTTP header to Nginx configuration
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],  # Set the HOST environment variable
-  command     => 'sudo sed -i '\''s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By "$HOST";/'\'' /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+-> file_line { 'add_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-# Restart Nginx service
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+-> exec {'restart_nginx':
+  command => '/usr/sbin/service nginx restart',
 }

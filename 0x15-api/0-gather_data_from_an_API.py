449a2d4eab6935adff85a00aa5a98db0161e98ff
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-30 01:25:33 +0000

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,13 +10,13 @@ from sys import argv
 
 if __name__ == "__main__":
 
-     url = "https://jsonplaceholder.typicode.com/"
-     user = requests.get(url + "users/{}".format(argv[1])).json().get('name')
-     todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
-     todoList = []
-     for item in todo:
-          if item.get('completed') is True:
-               todoList.append(item.get('title'))
-     print("Employee {} is done with tasks({}/{}):".format(
-          user, len(todoList), len(todo)))
-     [print("\t {}".format(item)) for item in todoList]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json().get('name')
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    todoList = []
+    for item in todo:
+        if item.get('completed') is True:
+            todoList.append(item.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+         user, len(todoList), len(todo)))
+    [print("\t {}".format(item)) for item in todoList]

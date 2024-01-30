0a964242e408e3cede73a6d43bfeeeb50f36fa24
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-30 02:28:27 +0000

    style fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,11 +10,14 @@ import requests
 
 def fetch_all_todo_data():
     """Fetch data from the API"""
-    users_data = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    todos_data = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+    users_data = requests.get(
+        "http://jsonplaceholder.typicode.com/users").json()
+    todos_data = requests.get(
+        "http://jsonplaceholder.typicode.com/todos").json()
 
     users = [(user.get('id'), user.get('username')) for user in users_data]
-    task_status_title = [(todo.get('userId'), todo.get('completed'), todo.get('title')) for todo in todos_data]
+    task_status_title = [(todo.get('userId'), todo.get('completed'),
+                          todo.get('title')) for todo in todos_data]
 
     return users, task_status_title
 
@@ -27,12 +30,14 @@ def export_to_json(users_data, todos_data):
         tasks = []
         for task in todos_data:
             if task[0] == user[0]:
-                tasks.append({"task": task[2], "completed": task[1], "username": user[1]})
+                tasks.append({"task": task[2], "completed": task[1],
+                              "username": user[1]})
         data[str(user[0])] = tasks
 
     filename = "todo_all_employees.json"
     with open(filename, "w") as json_file:
         json.dump(data, json_file, sort_keys=True, indent=2)
+
 
 if __name__ == "__main__":
     users, todos = fetch_all_todo_data()

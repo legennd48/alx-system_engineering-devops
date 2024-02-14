d0fa09ab164b484ad86dcd7f8854afa3e09eb395
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-02-14 16:34:05 +0000

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # puppet to automate fixing errors in server
 
 exec { 'fix typo':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => shell,
 }

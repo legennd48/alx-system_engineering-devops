a1ea97495daedb75517c4527d5bd6df21a89e9dc
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-02-22 09:31:26 +0000

    new pup

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
-# increase request limit at nginx
+# Fix problem of high amount of requests
 
-exec { 'fix nginx':
-  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
-  provider => shell
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# Restart Nginx
--> exec { 'restart nginx':
-  command  => 'sudo nginx restart',
-  provider => bash
-}
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',

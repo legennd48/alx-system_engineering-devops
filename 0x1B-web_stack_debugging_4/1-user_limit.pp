b17359c0ecc29a13ed7682be29aac53f91067c72
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-02-22 09:28:13 +0000

    puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -6,6 +6,6 @@ exec { 'fix limit hard':
 }
 
 exec { 'fix limit soft':
-  command   => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  provider  => shell
+  command  => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  provider => shell
 }
